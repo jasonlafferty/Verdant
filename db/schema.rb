91070d899efbeaa30c714528aec37696a3d0919f
Jason Lafferty <jason.lafferty@wayveapp.com>
2016-05-16 19:45:06 +0100

    added paper clip avatar to database, and other fields for user

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,11 @@
 ActiveRecord::Schema.define(version: 20160516180343) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "name"
+    t.string   "phone_number",           default: "", null: false
+    t.string   "address",                default: "", null: false
+    t.string   "email",                  default: "", null: false, unique: true
+    t.string   "avatar"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -31,4 +35,5 @@ ActiveRecord::Schema.define(version: 20160516180343) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  create_table "farms", force: .
 end
